b96e94f83ac9a63de6137b4c80a1775a6c58c9e8
Peter Ammundsen <peterammundsen@gmail.com>
2025-09-02 14:24:15 +0200

    use inner_product in norm2 function

--- LinearEquationSolver/src/MathUtils.cpp
+++ LinearEquationSolver/src/MathUtils.cpp
@@ -4,20 +4,14 @@
 
 #include "MathUtils.h"
 #include <cmath>
+#include <numeric>
 #include <stdexcept>
 #include <vector>
 
 
 double MathUtils::norm2(const std::vector<double>& x,
-                    const std::vector<double>& y) {
-    if (x.size() != y.size()) {
-        throw std::invalid_argument("Math::Utils::norm2: vectors must have same size");
-    }
-    double sum_sq = 0.0;
-    for (std::size_t i = 0; i < x.size(); ++i)
-    {
-        const double d = x[i] - y[i];
-        sum_sq += d * d;
-    }
-    return std::sqrt(sum_sq);
+                    const std::vector<double>& y)
+{
+    auto dq = std::inner_product(x.begin(), x.end(), y.begin(), 0.0);
+    return std::sqrt(dq);
 }
