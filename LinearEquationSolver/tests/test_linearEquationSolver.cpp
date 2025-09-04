bdc1a8ab136125e90323c6e98d2cd10e4138c838
fjaschmoneit <fynnaschmoneit@pm.me>
2025-09-04 10:26:29 +0200

    new tests for dense and sparse eqs, possible to test large matrix systems

--- LinearEquationSolver/tests/test_linearEquationSolver.cpp
+++ LinearEquationSolver/tests/test_linearEquationSolver.cpp
@@ -3,42 +3,47 @@
 #include "sparseMatrixSolvers.h"
 #include "denseMatrixSolvers.h"
 
-struct linearEquationsFixture : public ::testing::Test
+void fillBandInDenseMatrix(std::vector<std::vector<double>>& A, const std::vector<double>& q, int bandOffset)
 {
-    //    ( 2 1 0 0 0 0 )    ( x_0 )       (1)
-    //    ( 0 2 1 0 0 0 )    ( x_1 )       (2)
-    //    ( 0 0 2 1 0 0 )    ( x_2 )       (1)
-    //    ( 0 0 0 2 1 0 )  * ( x_3 )    =  (2)
-    //    ( 0 0 0 0 2 1 )    ( x_4 )       (1)
-    //    ( 0 0 0 0 0 2 )    ( x_5 )       (2)
-    //
-    // solution: x = (0, 1, 0, 1, 0, 1)ˆT
+    int n = A.size();
+    for (int i = 0; i < n; ++i)
+    {
+        int j = i + bandOffset;
+        if (j >= 0 && j < n)
+        {
+            A[i][j] = q[i];
+        }
+    }
+}
 
-    int N = 0;
+//    ( 2 1 0 0 0 0 )    ( x_0 )       (1)
+//    ( 0 2 1 0 0 0 )    ( x_1 )       (2)
+//    ( 0 0 2 1 0 0 )    ( x_2 )       (1)
+//    ( 0 0 0 2 1 0 )  * ( x_3 )    =  (2)
+//    ( 0 0 0 0 2 1 )    ( x_4 )       (1)
+//    ( 0 0 0 0 0 2 )    ( x_5 )       (2)
+//
+// solution: x = (0, 1, 0, 1, 0, 1)ˆT
+struct sparse_N6 : public ::testing::Test
+{
+    const int N = 6;
+
     std::vector<std::vector<double>> A;
     std::vector<double> b;
     std::vector<double> solution;
 
-    void SetUp()
+    sparse_N6()
+    : A(N, std::vector<double>(N)),
+    b(N, 1.0),
+    solution(N,0.0)
     {
-        N = 6;
-        A.assign(N, std::vector<double>(N, 0.0));
-        b.assign(N, 1.0);
-        solution.assign(N, 0.0);
+        fillBandInDenseMatrix(A, std::vector<double>(N, 2), 0);
+        fillBandInDenseMatrix(A, std::vector<double>(N-1, 1), 1);
 
         for (int i = 0; i < N; i++)
         {
-            A[i][i] = 2.0;
-            if (i != N-1)
-                A[i][i+1] = 1.0;
-            if (i % 2 != 0)
+            if (i % 2 != 0) {
                 b[i]++;
-        }
-        solution.resize(N);
-        for (int i = 0; i < N; i++)
-        {
-            if (i%2 != 0)
-            {
                 solution[i]++;
             }
         }
@@ -46,32 +51,111 @@ struct linearEquationsFixture : public ::testing::Test
 };
 
 
-TEST_F(linearEquationsFixture, solveDenseJacobiLinearSystem)
+//    ( -2 0 0 0 1 0 0 0 0 0 0 0 ...)    ( x_0 )       (3)
+//    ( 0 -2 0 0 0 1 0 0 0 0 0 0 ...)    ( x_1 )       (2)
+//    ( 0 0 -2 0 0 0 1 0 0 0 0 0 ...)    ( x_2 )       (1)
+//    ( 0 0 0 -2 0 0 0 1 0 0 0 0 ...)  * ( x_3 )    =  (0)
+//    ( 1 0 0 0 0 -2 0 0 0 1 0 0 ...)    ( x_4 )       (0)
+//    ( 0 1 0 0 0 0 -2 0 0 0 1 0 ...)    ( x_5 )       (0)
+//              ......
+//    ( ... 0 0 0 1 0 0 0 -2 0 0 0 1)    ( x_N-5 )      (0)
+//    ( ... 0 0 0 0 1 0 0 0 -2 0 0 0)    ( x_N-4 )      (-N-1)
+//    ( ... 0 0 0 0 0 1 0 0 0 -2 0 0)    ( x_N-3 )      (-N-2)
+//    ( ... 0 0 0 0 0 0 1 0 0 0 -2 0)    ( x_N-2 )      (-N-3)
+//    ( ... 0 0 0 0 0 0 0 1 0 0 0 -2)    ( x_N-1 )      (-N-4)
+
+
+// solution: x = (1, 2, 3, 4, ... , N)ˆT
+struct sparse_N : public ::testing::Test
 {
+    std::vector<std::vector<double>> A;
+    std::vector<double> b;
+    std::vector<double> solution;
 
-    linearEquationsFixture::SetUp();
+    void setProblemSize( const int N) {
+        A.assign(N, std::vector<double>(N,0.0));
+        b.assign(N,0.0);
+        solution.assign(N,0.0);
 
-    Dense::JacobiIter linEqs(A,b);
+        fillBandInDenseMatrix(A, std::vector<double>(N, -2.0), 0);
+        fillBandInDenseMatrix(A, std::vector<double>(N, 1.0), 4);
+        fillBandInDenseMatrix(A, std::vector<double>(N, 1.0), -4);
 
-    const std::vector<double> x = linEqs.solve();
-    EXPECT_EQ(x, solution);
-}
+        b[0] = 3.0;
+        b[1] = 2.0;
+        b[2] = 1.0;
+        b[N-1] = -N-4.0;
+        b[N-2] = -N-3.0;
+        b[N-3] = -N-2.0;
+        b[N-4] = -N-1.0;
 
-TEST_F(linearEquationsFixture, solveGaussSeidelDenseLinearSystem)
-{
+        std::iota(solution.begin(), solution.end(), 1.0);
+    }
+};
 
-    linearEquationsFixture::SetUp();
 
+
+//    Q = 0.5*N*(N+1)
+//
+//    ( 2 1 1 1 1 ... )    ( x_0 )       ( 1 + Q )
+//    ( 1 2 1 1 1 ... )    ( x_1 )       ( 2 + Q )
+//    ( 1 1 2 1 1 ... )    ( x_2 )   =   ( 3 + Q )
+//    ( 1 1 1 2 1 ... )    ( x_3 )       ( 4 + Q )
+//           ...              ...           ...
+//    ( ... 1 1 1 1 2 )    ( x_(N-1) )   ( N + Q )
+//
+// solution: x = (1, 2, 3, 4, ... , N)ˆT
+struct dense_N : public ::testing::Test {
+    std::vector<std::vector<double>> A;
+    std::vector<double> b;
+    std::vector<double> solution;
+
+    void setProblemSize(const int N) {
+
+        A.assign(N, std::vector<double>(N,1.0));
+        fillBandInDenseMatrix(A, std::vector<double>(N, 2.0), 0);
+
+        b.assign(N,0.0);
+        std::iota(b.begin(), b.end(), 1+0.5*N*(N+1) );
+
+        solution.assign(N,0.0);
+        std::iota(solution.begin(), solution.end(), 1.0);
+    }
+};
+
+TEST_F(dense_N, denseSmallGaussSeidel) {
+    setProblemSize(60);         //
     Dense::GaussSeidel linEqs(A,b);
-    const std::vector<double> x = linEqs.solve();
-  
+    auto x = linEqs.solve();
+    EXPECT_EQ(x, solution);
+}
+
+TEST_F(sparse_N, denseMidSizeJacobi)
+{
+    setProblemSize(20);
+    Dense::JacobiIter linEqs(A,b);
+    auto x = linEqs.solve();
     EXPECT_EQ(x, solution);
 }
 
 
 
+TEST_F(sparse_N6, solveDenseJacobiLinearSystem)
+{
+    Dense::JacobiIter linEqs(A,b);
+    EXPECT_EQ(linEqs.solve(), solution);
+}
 
-TEST_F(linearEquationsFixture, solveSparseLinearSystem){
+TEST_F(sparse_N6, solveGaussSeidelDenseLinearSystem)
+{
+    Dense::GaussSeidel linEqs(A,b);
+    EXPECT_EQ(linEqs.solve(), solution);
+}
+
+
+
+
+TEST_F(sparse_N6, solveSparseLinearSystem){
 
     int N = 10;
 
