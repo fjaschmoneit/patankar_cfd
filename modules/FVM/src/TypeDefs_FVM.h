b6e8b846c43d561aae94771bf955f5ef22b5a2ae
Peter Ammundsen <peterammundsen@gmail.com>
2026-01-06 10:57:11 +0100

    clean up code

--- modules/FVM/src/TypeDefs_FVM.h
+++
@@ -1,5 +0,0 @@
-namespace FVM
-{
-    using scalar = double;
-    enum class CardinalDirection {centre, west, east, north, south };
-}
--- modules/NumericsKernel/include/KernelTypeDefs.h
+++ modules/NumericsKernel/include/KernelTypeDefs.h
@@ -1,14 +1,6 @@
 #include <blaze/Blaze.h>
 
 namespace KERNEL {
-
-    using scalar = double;
-// #ifndef KERNEL_SCALAR_T
-//     using scalar = double;              // default
-// #else
-//     using scalar = KERNEL_SCALAR_T;     // define this in cmake file
-// #endif
-
     using vector = blaze::DynamicVector<scalar, blaze::columnVector>;
     using smatrix = blaze::CompressedMatrix<scalar, blaze::rowMajor>;
     using dmatrix = blaze::DynamicMatrix<scalar, blaze::rowMajor>;
